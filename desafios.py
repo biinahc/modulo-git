6bec217fec4060bf485fc67da2b32df961180cb8
kennedyHenrique <kennedy.henoliveira@gmail.com>
2025-11-05 20:19:14 -0400

    subindo desafio4

--- desafios.py
+++ desafios.py
@@ -21,12 +21,28 @@ print(f"{criar_mensagem_commit({listar_comandos_git_basicos()})}")
 
 print('---'*10)
 
-def verificar_tag_valida(tag):
-    """
-    Verifica se uma tag está no formato 'vX.Y' (ex: v1.0, v2.1).
-    Retorna True se o formato for válido, caso contrário False.
-    """
-    pass
+def verificar_tag_valida_sem_regex(tag):    
+    if not tag.startswith('v'):
+        return False        
+    conteudo_versao = tag[1:]    
+    partes = conteudo_versao.split('.')
+
+    if len(partes) != 2:
+        return False
+        
+    major, minor = partes
+    
+    if not major or not minor: 
+        return False
+    return major.isdigit() and minor.isdigit()
+
+
+print(f"verificar_tag_valida_sem_regex('v1.0'): {verificar_tag_valida_sem_regex('v1.0')}") 
+print(f"verificar_tag_valida_sem_regex('v2.10'): {verificar_tag_valida_sem_regex('v2.10')}") 
+print(f"verificar_tag_valida_sem_regex('v1.0.1'): {verificar_tag_valida_sem_regex('v1.0.1')}") 
+print(f"verificar_tag_valida_sem_regex('1.0'): {verificar_tag_valida_sem_regex('1.0')}") 
+print(f"verificar_tag_valida_sem_regex('v1.a'): {verificar_tag_valida_sem_regex('v1.a')}") 
+print(f"verificar_tag_valida_sem_regex('v.'): {verificar_tag_valida_sem_regex('v.')}") 
 
 print('---'*10)
 
